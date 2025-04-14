a8dbebc71fb716a100c9560a786c9bafb1911ccf
DragonaC.B <Dragona163@bk.ru>
2025-04-15 04:08:41 +0500

    Add gitignore

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -17,7 +17,6 @@ public class MainApp {
       AnnotationConfigApplicationContext context =
             new AnnotationConfigApplicationContext(AppConfig.class);
 
-
       UserService userService = context.getBean(UserService.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Toyota", 2020)));
